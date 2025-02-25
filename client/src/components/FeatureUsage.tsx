7470f1c976cb02e62b2e999d20c49227053f6859
pszafo <ps@zafo.in>
2025-02-25 10:22:49 +0530

    Create FeatureUsage.tsx

---
+++ client/src/components/FeatureUsage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FeatureUsage: React.FC = () => {
+  return (
+    <div>
+      <h1>Feature Usage</h1>
+      <p>Analyze the usage of different features in your application.</p>
+    </div>
+  );
+};
+
+export default FeatureUsage;
