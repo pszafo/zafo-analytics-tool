e644dea94136d03d7fc17e1c725bd82ed52c0bc9
pszafo <ps@zafo.in>
2025-02-25 10:22:11 +0530

    Create ChurnAnalysis.tsx

---
+++ client/src/components/ChurnAnalysis.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ChurnAnalysis: React.FC = () => {
+  return (
+    <div>
+      <h1>Churn Analysis</h1>
+      <p>Analyze churn risk and identify potential churners.</p>
+    </div>
+  );
+};
+
+export default ChurnAnalysis;
