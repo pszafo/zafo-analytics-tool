9ffe2208dbefdfc7c384bb32eab5b9c04629a682
pszafo <ps@zafo.in>
2025-02-25 10:21:36 +0530

    Create UserTracking.tsx

---
+++ client/src/components/UserTracking.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserTracking: React.FC = () => {
+  return (
+    <div>
+      <h1>User Tracking</h1>
+      <p>Track user movements and interactions within your application.</p>
+    </div>
+  );
+};
+
+export default UserTracking;
