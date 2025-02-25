13bb170a2b8679b18f374e13c304ae0d48422f01
pszafo <ps@zafo.in>
2025-02-25 10:23:49 +0530

    Create FeatureRequests.tsx

---
+++ client/src/components/FeatureRequests.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FeatureRequests: React.FC = () => {
+  return (
+    <div>
+      <h1>Feature Requests</h1>
+      <p>Gather and prioritize new feature requests from users.</p>
+    </div>
+  );
+};
+
+export default FeatureRequests;
